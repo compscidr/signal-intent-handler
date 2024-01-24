f469700768c3c92fb0234e9568cba9a0f44fde81
Jason Ernst <ernstjason1@gmail.com>
2024-01-24 09:58:32 -0800

    fix failing build

--- sampleintentreceiver/src/main/java/com/jasonernst/sampleintentreceiver/MainActivity.java
+++ sampleintentreceiver/src/main/java/com/jasonernst/sampleintentreceiver/MainActivity.java
@@ -2,8 +2,12 @@ package com.jasonernst.sampleintentreceiver;
 
 import static com.jasonernst.sampleintentreceiver.InstrumentedTestReceiver.XHIST_INTENT;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.IntentFilter;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     //this need to be added to RightMeshMainActivity
     InstrumentedTestReceiver instrumentedTestReceiver = new InstrumentedTestReceiver();
 
+    @SuppressLint("UnspecifiedRegisterReceiverFlag")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
         //this should be stuck into the main activity of the RightMeshController
         //ie: right after the onCreate in RightMeshMainActivity
-        getApplicationContext().registerReceiver(instrumentedTestReceiver,
-                new IntentFilter(XHIST_INTENT));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            getApplicationContext().registerReceiver(instrumentedTestReceiver,
+                    new IntentFilter(XHIST_INTENT), Context.RECEIVER_NOT_EXPORTED);
+        } else {
+            getApplicationContext().registerReceiver(instrumentedTestReceiver,
+                    new IntentFilter(XHIST_INTENT));
+        }
     }
 
     @Override
